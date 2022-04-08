fb224c8bb2acaefda54c1163cc826c92bde1064a
silvaij <isaque.josias@gmail.com>
2022-04-08 18:15:48 -0300

    Primeiro commit inicio do projeto

---
+++ gamaster/src/main/java/br/edu/gama/gamaster/GamasterApplication.java
@@ -0,0 +1,13 @@
+package br.edu.gama.gamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamasterApplication.class, args);
+	}
+
+}
---
+++ gamaster/src/test/java/br/edu/gama/gamaster/GamasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.gama.gamaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
