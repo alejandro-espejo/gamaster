cda66c85561c66c93c594c9864f27f08725c5b72
WesleyOliver <wesley.devfullstack@gmail.com>
2022-04-19 17:15:52 -0300

    Adicionado o JUnit no projeto e implementado um simples teste de busca de cliente pelo codigo.

--- gamaster/src/main/java/br/edu/gama/gamaster/model/Cliente.java
+++ gamaster/src/main/java/br/edu/gama/gamaster/model/Cliente.java
@@ -1,5 +1,7 @@
 package br.edu.gama.gamaster.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -19,7 +21,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Cliente {
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
