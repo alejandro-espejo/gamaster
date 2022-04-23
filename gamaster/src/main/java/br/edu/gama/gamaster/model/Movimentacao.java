2ebbe2b991c07ac5266db741545847be58a3ca12
Thiago Felipe <thiagofg.eng@gmail.com>
2022-04-23 11:44:19 -0300

    Correção do saldo nulo e enum do tipo da conta

--- gamaster/src/main/java/br/edu/gama/gamaster/model/Movimentacao.java
+++ gamaster/src/main/java/br/edu/gama/gamaster/model/Movimentacao.java
@@ -4,15 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
@@ -38,7 +30,7 @@ public class Movimentacao implements Serializable {
     private Long codigo;
     
     @NotNull
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     @Column(name = "tipo_movimentacao")
     private TipoMovimentacao tipoMovimentacao;
 
--- gamaster/src/main/resources/db/migration/V002__criar_e_registrar_conta.sql
+++ gamaster/src/main/resources/db/migration/V002__criar_e_registrar_conta.sql
@@ -12,20 +12,20 @@ CREATE TABLE tb_conta (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, limite_de_credito_pre_aprovado, cod_cliente)
-VALUES('CC', '0365', '2022-04-22', '159874-6', 'CONTA_CORRENTE', null, 1);
+INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, saldo, limite_de_credito_pre_aprovado, cod_cliente)
+VALUES('CC', '0365', '2022-04-22', '159874-6', 'CONTA_CORRENTE', 0, 0, 1);
 
-INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, limite_de_credito_pre_aprovado, cod_cliente)
-VALUES('CC', '0365', '2022-04-22', '854962-6', 'CONTA_CORRENTE', null, 2);
+INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, saldo, limite_de_credito_pre_aprovado, cod_cliente)
+VALUES('CC', '0365', '2022-04-22', '854962-6', 'CONTA_CORRENTE', 0, 0, 2);
 
-INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, limite_de_credito_pre_aprovado, cod_cliente)
-VALUES('CC', '0365', '2022-04-22', '369147-6', 'CONTA_CORRENTE', null, 3);
+INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, saldo, limite_de_credito_pre_aprovado, cod_cliente)
+VALUES('CC', '0365', '2022-04-22', '369147-6', 'CONTA_CORRENTE', 0, 0, 3);
 
-INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, limite_de_credito_pre_aprovado, cod_cliente)
-VALUES('CE', '0365', '2022-04-22', '851369-6', 'CONTA_ESPECIAL', 300, 4);
+INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, saldo, limite_de_credito_pre_aprovado, cod_cliente)
+VALUES('CE', '0365', '2022-04-22', '851369-6', 'CONTA_ESPECIAL', 300, 0, 4);
 
-INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, limite_de_credito_pre_aprovado, cod_cliente)
-VALUES('CE', '0365', '2022-04-22', '741589-6', 'CONTA_ESPECIAL', 300, 5);
+INSERT INTO tb_conta (dtype, agencia, data_criacao, numero_conta, tipo_conta, saldo, limite_de_credito_pre_aprovado, cod_cliente)
+VALUES('CE', '0365', '2022-04-22', '741589-6', 'CONTA_ESPECIAL', 300, 0, 5);
 
 
 
